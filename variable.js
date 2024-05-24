b3fbc0b6160b5026f3317282a063649e7701deae
ksinsinwar <karishma.sinsinwar@offsoar.com>
2024-05-24 22:30:43 +0530

    difference between let,const and var

---
+++ variable.js
@@ -0,0 +1,12 @@
+const accountId=12365
+var accountEmail="karis@gmail.com"
+let accountPassword="466321"
+accountCity="Kannur"
+
+//accountId=6
+accountEmail="Karishma@gmail.com"
+accountPassword="7996624"
+accountCity="Kanpur"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
