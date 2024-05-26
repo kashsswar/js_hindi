8ad112f1241603b6217c177d595a61223f25d0b6
ksinsinwar <karishma.sinsinwar@offsoar.com>
2024-05-26 23:06:15 +0530

    Difference betwen stack and heap

--- dataType-summary.js
+++ dataType-summary.js
@@ -16,29 +16,56 @@
 //3. function
 
 
-const score=100
-const scoreValue=100.3
+// const score=100
+// const scoreValue=100.3
 
-const isLoggedIn=false
-const outsideTemp=null
-let userEmail;
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
 
-const id=Symbol('123')
-const anotherId=Symbol('123')
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
 
-console.table([id,anotherId])
-console.log(id===anotherId)
+// console.table([id,anotherId])
+// console.log(id===anotherId)
 
-const bigNumber=46565622n
+// const bigNumber=46565622n
 
 
-const arr=["banana","apple","grapes"]
+// const arr=["banana","apple","grapes"]
 
-const myObject={
-    name:"karishma",
-    age:22
-}
+// const myObject={
+//     name:"karishma",
+//     age:22
+// }
 
-const myFunction=function(){
-    console.log("Hello world")
-}
+// const myFunction=function(){
+//     console.log("Hello world")
+// }
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+ //Stack (Primitive), Heap (Non-Primitive)
+
+//Primitve mein change karne par uski copy mein change hota , jabki non-primitive mein uska original ka reference milta hai....toh non-primitive change 
+//seedhe uske original mein hota
+
+// let myName="Karishma"
+// let myOtherName=myName
+
+// myOtherName="Bulbul"
+
+// console.table([myName, myOtherName])
+
+let myObject = {
+    userEmail: "karish@gmail.com",
+    userPassword: "123456"
+};
+let myOtherObject = myObject;
+myOtherObject.userEmail = "bulbul@gmail.com";
+
+console.table([myObject, myOtherObject]);
