1cd3cd2bab817c5aec34394e9234844f65468d9b
ksinsinwar <karishma.sinsinwar@offsoar.com>
2024-05-27 20:14:32 +0530

    Array- part1 methods implemented

---
+++ arrays.js
@@ -0,0 +1,48 @@
+// const myAr=[0,1,2,3,4,5]
+
+// console.log(myAr)
+
+// console.log(myAr.length)
+//  console.log(myAr[1])
+
+const myArr2= new Array(1,2,3,4)
+
+// console.log(myArr2[0])
+
+// myArr2.push(6)
+// console.log(myArr2)
+
+// myArr2.push(7)
+// console.log(myArr2)
+
+// myArr2.pop()
+// console.log(myArr2)
+
+// myArr2.unshift(9)
+// console.log(myArr2)
+
+// myArr2.shift()
+// console.log(myArr2)
+
+// console.log(myArr2.includes(9))
+
+// console.log(myArr2.indexOf(9))
+
+// console.log(myArr2.indexOf(6))
+
+// const newArr= myArr2.join()
+// console.log(newArr)
+
+// console.log(typeof newArr)
+
+//slice doesnt make changes in the original while splice does
+
+// console.log(myArr2)
+// const myn1=myArr2.slice(1,3)
+// console.log(myn1)
+// console.log("A", myArr2)
+
+// const myn2=myArr2.splice(1,3)
+// console.log(myn2)
+
+// console.log("B",myArr2)
