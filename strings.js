7d61123cf1b909f8f3f8fa879c1296c11e712623
ksinsinwar <karishma.sinsinwar@offsoar.com>
2024-05-27 10:25:43 +0530

    Implemented string methods

---
+++ strings.js
@@ -0,0 +1,33 @@
+const name="Karishma"
+const repoCount=50
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`)
+
+const gameName=new String("Karishma-sc-kl-pd-lover")
+// console.log(gameName)
+
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt("5"))
+
+// console.log(gameName.indexOf("i"))
+
+// console.log(gameName.substring(2,6))
+
+// console.log(gameName.slice(-7,8))
+
+let str="  Aditya Sinsinwar  "
+
+// console.table([str, str.trim()])
+
+const url="https://www.instagram%20.com"
+
+// console.log(url.replace("%20","-"))
+
+// console.log(url.includes("instag"))
+
+console.table([url.split("."),gameName.split("-")])
