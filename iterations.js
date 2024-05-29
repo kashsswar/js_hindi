766260a7c41502bf4604405333db2e0b60abbc8d
ksinsinwar <karishma.sinsinwar@offsoar.com>
2024-05-29 12:26:26 +0530

    iterations done

---
+++ iterations.js
@@ -0,0 +1,84 @@
+// for (let i=0; i<=10; i++){
+//     let element =i
+
+//     if(element==5){
+//         console.log("Detected 5");
+//     }
+//     console.log(element);
+    
+// }
+
+
+// for(let i=0; i<=10; i++){
+//     console.log("Outer loop", i);
+//     for(let j=0; j<=10; j++){
+//         console.log(`Inner loop value is ${j} in outer loop ${i}` );
+//     }
+// }
+
+// for(let i=0; i<=10; i++){
+//     console.log("Outer loop", i);
+//     for(let j=0; j<=10; j++){
+ //     }
+// }
+
+// let myArray=["flash", "batman", "ironman"]
+
+// for(let i=0; i<myArray.length; i++){
+//     console.log(myArray[i]);
+// }
+
+//++++++++++++++++++++++++break and continue+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// for (let index=1; index<=20; index++){
+//     if(index==5){
+//         console.log("Detected 5");
+//         break
+//     }
+//     console.log(`Value of i = ${index}`);
+// }
+
+// for (let index=1; index<=20; index++){
+//     if(index==5){
+//         console.log("Detected 5");
+//         continue
+//     }
+//     console.log(`Value of i = ${index}`);
+// }
+
+//+++++++++++++++++++++++++++++++++++while loop+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let index=0;
+// while(index<=10){
+//     console.log(index);
+//     index=index+2;
+// }
+
+// let myArray=["flash", "superman", "ironman"]
+
+// let index=0
+
+// while(index<myArray.length){
+//     console.log(myArray[index]);
+//     index=index+1
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++do-whle+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let score=1
+
+// do{
+//     console.log(score);
+//     score=score+1
+// }
+// while(score<=10)
+
+//hum do-while ko jyada kam mein nahi lete hain....iska example below++++++++++++++++++++++++++++++++
+
+let score=11
+
+do{
+    console.log(score);
+    score=score+1
+}
+while(score<=10)
